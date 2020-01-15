773a93042edc69e717b97f8540be583375072467
Handowsblack <49844903+Handowsblack@users.noreply.github.com>
2020-01-15 01:00:43 -0300

    Commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Lex:1234567890handows@cluster0-iiicg.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333, console.log("Rodando na porta 3333"));
+ 
+
+// Métodos HTTP: get, post, put, delete 
+//Query Params:req.query (filtros, ordenação, paginação, ...)
+//Route Params:
+//Body: request.body 
+
+// MongoDB
